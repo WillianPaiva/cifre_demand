8363ec149fbc26a703e362bed88e42779f015162
willianPaiva <vervalenpaiva@gmail.com>
2017-10-03 13:17:24 +0200

    better state of art

--- auto/main.el
+++ auto/main.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,13 +32,13 @@
     "hyperref"
     "minted")
    (LaTeX-add-labels
-    "sec:org9cd067c"
-    "sec:orgd3a24ca"
-    "sec:org15c6af8"
-    "sec:org509d72d"
-    "sec:org151988f"
-    "sec:orgbd708cc"
-    "sec:orgf9a9080")
+    "sec:org6257aa9"
+    "sec:orgacc38c5"
+    "sec:org6b11f7d"
+    "sec:org4c0a91d"
+    "sec:org5e5ac54"
+    "sec:org4ca33dd"
+    "sec:orgf531699")
    (LaTeX-add-bibliographies
     "repport"))
  :latex)
