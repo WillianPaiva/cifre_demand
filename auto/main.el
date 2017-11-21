983b7f9b83109331d442c0f0a97cce9705c2ce2c
willianPaiva <vervalenpaiva@gmail.com>
2017-11-21 22:37:39 +0100

    corrections done

--- auto/main.el
+++ auto/main.el
@@ -2,7 +2,7 @@
  "main"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "11pt")))
+                     '(("article" "french")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
@@ -16,7 +16,7 @@
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art11"
+    "art10"
     "inputenc"
     "fontenc"
     "graphicx"
@@ -30,15 +30,16 @@
     "amssymb"
     "capt-of"
     "hyperref"
+    "babel"
     "minted")
    (LaTeX-add-labels
-    "sec:orged2ba66"
-    "sec:org478492c"
-    "sec:org00ed8cb"
-    "sec:orgc54e2f3"
-    "sec:orgcc64c7c"
-    "sec:org3727189"
-    "sec:orgcf02a1b")
+    "sec:orga8bb14f"
+    "sec:org8119c98"
+    "sec:orgcad02fb"
+    "sec:orgf2f9c8d"
+    "sec:orgb1aac1c"
+    "sec:org66b1492"
+    "sec:org2dc06f3")
    (LaTeX-add-bibliographies
     "repport"))
  :latex)
